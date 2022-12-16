e3d5ae6a780a0e7e0a0504c08cd14d685b195c9f
Daniël Hofman <d.hofman@st.hanze.nl>
2022-12-16 15:11:02 +0100

    details page update

--- svelte-app/src/routes/aanvragen/[requestId]/+page.svelte
+++ svelte-app/src/routes/aanvragen/[requestId]/+page.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
 	type Aanvraag = {
 		id: number;
-		// fromUser: string;
+		fromUser: string;
 		requestText: string;
 		date: number;
 	};
 	export let data: { items: Aanvraag[] };
 	const items = data.items;
+
+	function unixConvertion(unixTimestamp: number) {
+		const date: Date = new Date(unixTimestamp);
+		return date;
+	}
 </script>
 
-<h1>{items.id}</h1>
-<p>{items.requestText}</p>
+<main>
+	<h1>{items.fromUser.email}</h1>
+	<h3>{unixConvertion(items.date)}</h3>
+	<br />
+	<p>{items.requestText}</p>
+</main>
+
+<style>
+	main {
+		font-family: sans-serif;
+		text-align: center;
+	}
+</style>
