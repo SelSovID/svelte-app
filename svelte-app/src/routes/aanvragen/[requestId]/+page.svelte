367d424b56f49bbdebb895916d02a6f985eedc22
Daniël Hofman <d.hofman@st.hanze.nl>
2022-12-16 16:34:13 +0100

    empty accept/reject buttons with basic alert

--- svelte-app/src/routes/aanvragen/[requestId]/+page.svelte
+++ svelte-app/src/routes/aanvragen/[requestId]/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import IconButton from '@smui/icon-button';
+	import Fab, { Label, Icon } from '@smui/fab';
+
 	type Aanvraag = {
 		id: number;
 		fromUser: string;
 		requestText: string;
 		date: number;
 	};
+
+	let clicked = 0;
 	export let data: { items: Aanvraag[] };
 	const items = data.items;
 
@@ -14,11 +19,31 @@
 	}
 </script>
 
+<br />
+
+<div class="flexy">
+	<div class="margins">
+		<Fab on:click={() => clicked++} href="/aanvragen" extended>
+			<Icon class="material-icons">arrow_back</Icon>
+			<Label>terug</Label>
+		</Fab>
+	</div>
+</div>
+
 <main>
 	<h1>{items.fromUser.email}</h1>
 	<h3>{unixConvertion(items.date)}</h3>
 	<br />
 	<p>{items.requestText}</p>
+
+	<Fab color="primary" on:click={() => alert(items.fromUser.email + ' goedgekeurd')} extended>
+		<Icon class="material-icons">done</Icon>
+		<Label>goedkeuren</Label>
+	</Fab>
+	<Fab color="primary" on:click={() => alert(items.fromUser.email + ' afgekeurd')} extended>
+		<Icon class="material-icons">close</Icon>
+		<Label>afkeuren</Label>
+	</Fab>
 </main>
 
 <style>
